dafe4ed39dea6f52929cdafaf27650dff09dd39f
DanieleFlo <daniele.florio00@gmail.com>
2024-11-07 15:37:38 +0100

    Create domanda_4.py

---
+++ domanda_4.py
@@ -0,0 +1,7 @@
+#Cos'è la coerenza funzionale? Come la possiamo dimostrare?
+
+"""
+La coerenza funzionale è la capacità del paradigma dell' OOP di essere 
+eseguira sempre nello stesso modo, mantenendo le stesse funzionalità,
+a patto che il codice sia lo stesso.
+"""
